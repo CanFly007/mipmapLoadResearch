876d4e5b79bd624124abf6eb741fa72ff6553d2d
HeXueqiang <hexueqiang007@gmail.com>
2024-03-26 19:00:18 +0800

    assetBundle.Unload

--- Assets/Scripts/AssetBundleLoader.cs
+++ Assets/Scripts/AssetBundleLoader.cs
@@ -18,16 +18,26 @@ public class AssetBundleLoader : MonoBehaviour
 
     private GameObject teaportGo;
 
+    private static bool m_UnloadAB = true;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             m_loadMipmapLevel = 0;
+            m_UnloadAB = true;
             DeCompressBundleAsync().Forget();
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             m_loadMipmapLevel = 3;
+            m_UnloadAB = true;
+            DeCompressBundleAsync().Forget();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            m_loadMipmapLevel = 0;
+            m_UnloadAB = false;
             DeCompressBundleAsync().Forget();
         }
 
@@ -35,6 +45,10 @@ public class AssetBundleLoader : MonoBehaviour
         {
             if (teaportGo != null)
             {
+                ////test
+                //string assetBundlePath = Path.Combine(Application.streamingAssetsPath, "teaport");
+                //AssetBundle assetBundle = AssetBundle.LoadFromFile(assetBundlePath);
+
                 Texture2D texture = teaportGo.GetComponent<MeshRenderer>().sharedMaterial.mainTexture as Texture2D;
                 texture.ForceSetMipLevel(6);
                 //Debug.Log(texture.name);
@@ -45,9 +59,36 @@ public class AssetBundleLoader : MonoBehaviour
         {
             if (teaportGo != null)
             {
+                ////test
+                //string assetBundlePath = Path.Combine(Application.streamingAssetsPath, "teaport");
+                //AssetBundle.LoadFromFile(assetBundlePath);
+
                 Texture2D texture = teaportGo.GetComponent<MeshRenderer>().sharedMaterial.mainTexture as Texture2D;
                 texture.ForceSetMipLevel(0);
                 //Debug.Log(texture.name);
+            }
+        }
+
+        //GetPixel failed, because is not readable
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (teaportGo != null)
+            {
+                Texture2D texture = teaportGo.GetComponent<MeshRenderer>().sharedMaterial.mainTexture as Texture2D;
+                Color c = texture.GetPixel(0, 0);
+                Debug.Log(c);
+            }
+        }
+
+        //test LoadAB direct not async
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            string assetBundlePath = Path.Combine(Application.streamingAssetsPath, "teaport");
+            AssetBundle ab = AssetBundle.LoadFromFile(assetBundlePath);
+            Object[] objects = ab.LoadAllAssets();
+            for (int i = 0; i < objects.Length; i++)
+            {
+                teaportGo = Instantiate(objects[i] as GameObject);
             }
         }
     }
@@ -84,5 +125,7 @@ public class AssetBundleLoader : MonoBehaviour
         catch
         { 
         }
+        if(m_UnloadAB)
+            assetBundleCreateRequest.assetBundle.Unload(false);
     }
 }
