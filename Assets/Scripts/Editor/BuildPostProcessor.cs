e02c7d6476199f0a9ddb6cbf4b0db133704b99df
HeXueqiang <hexueqiang007@gmail.com>
2024-04-21 10:07:34 +0800

    将TextureCompress功能移植过来，支持astc和crunch，win全支持。todo 1、mac支持crunch，2、astc支持mip

---
+++ Assets/Scripts/Editor/BuildPostProcessor.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using System.IO;
+
+public class BuildPostProcessor : IPostprocessBuildWithReport
+{
+    public int callbackOrder { get { return 0; } }
+
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        //copy Tools/astcenc-sse2.exe to _Data/Tools/astcenc-sse2.exe
+        BuildTarget activeBuildTarget = EditorUserBuildSettings.activeBuildTarget;
+
+        string outputDir = Path.GetDirectoryName(report.summary.outputPath);
+        if (activeBuildTarget == BuildTarget.StandaloneOSX)
+        {
+            outputDir = Path.Combine(outputDir, Path.GetFileNameWithoutExtension(report.summary.outputPath) + ".app");
+        }
+
+        string exeName = activeBuildTarget == BuildTarget.StandaloneOSX ? "astcenc-sse2-arm64" : "astcenc-sse2.exe";
+
+        string sourcePath = Path.Combine(Application.dataPath, "Tools", exeName);
+        string destinationToolsDir = Path.Combine(outputDir, Application.productName + "_Data", "Tools");
+        string targetPath = Path.Combine(destinationToolsDir, exeName);
+
+        if (activeBuildTarget == BuildTarget.StandaloneOSX)
+        {
+            string destinationEncoderDir = Path.Combine(report.summary.outputPath, "Contents", "Tools");
+            targetPath = Path.Combine(destinationEncoderDir, exeName);
+        }
+
+        if (!Directory.Exists(Path.GetDirectoryName(targetPath)))
+            Directory.CreateDirectory(Path.GetDirectoryName(targetPath));
+
+        if (File.Exists(sourcePath))
+            File.Copy(sourcePath, targetPath, true);
+        else
+            Debug.LogError("Tool not found: " + sourcePath);
+
+
+
+        //copy Tools/crunch.exe to _Data/Tools/crunch.exe
+        //BuildTarget activeBuildTarget = EditorUserBuildSettings.activeBuildTarget;
+
+        //string outputDir = Path.GetDirectoryName(report.summary.outputPath);
+        //if (activeBuildTarget == BuildTarget.StandaloneOSX)
+        //{
+        //    outputDir = Path.Combine(outputDir, Path.GetFileNameWithoutExtension(report.summary.outputPath) + ".app");
+        //}
+
+        exeName = "crunch.exe";
+
+        sourcePath = Path.Combine(Application.dataPath, "Tools", exeName);
+        destinationToolsDir = Path.Combine(outputDir, Application.productName + "_Data", "Tools");
+        targetPath = Path.Combine(destinationToolsDir, exeName);
+
+        //if (activeBuildTarget == BuildTarget.StandaloneOSX)
+        //{
+        //    string destinationEncoderDir = Path.Combine(report.summary.outputPath, "Contents", "Tools");
+        //    targetPath = Path.Combine(destinationEncoderDir, exeName);
+        //}
+
+        if (!Directory.Exists(Path.GetDirectoryName(targetPath)))
+            Directory.CreateDirectory(Path.GetDirectoryName(targetPath));
+
+        if (File.Exists(sourcePath))
+            File.Copy(sourcePath, targetPath, true);
+        else
+            Debug.LogError("Tool not found: " + sourcePath);
+
+
+
+        // 复制图片
+        string imageName = "RGBA32.png";
+        string sourcePathImage = Path.Combine(Application.dataPath, "Images", imageName);
+        string destinationImagesDir = Path.Combine(outputDir, Application.productName + "_Data", "Images");
+        string targetPathImage = Path.Combine(destinationImagesDir, imageName);
+        if (!Directory.Exists(Path.GetDirectoryName(targetPathImage)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(targetPathImage));
+        }
+        if (File.Exists(sourcePathImage))
+        {
+            File.Copy(sourcePathImage, targetPathImage, true);
+        }
+        else
+        {
+            Debug.LogError("Image not found: " + sourcePathImage);
+        }
+    }
+}
---
+++ Assets/Scripts/TextureCompress.cs
@@ -0,0 +1,148 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class TextureCompress : MonoBehaviour 
+{
+    private string toolPath;
+    private string pngPath;
+
+    private string crunchToolPath;
+
+    void Start()
+    {
+        string appData = Path.Combine(Application.dataPath, "Tools");
+        switch (Application.platform)
+        {
+            case RuntimePlatform.WindowsPlayer:
+            case RuntimePlatform.WindowsEditor:
+                toolPath = Path.Combine(appData, "astcenc-sse2.exe");
+                break;
+            case RuntimePlatform.OSXPlayer:
+            case RuntimePlatform.OSXEditor:
+                toolPath = Path.Combine(appData, "astcenc-sse2-arm64");
+                break;
+        }
+        if (!File.Exists(toolPath))
+            throw new FileNotFoundException($"Cannot find astc encoder at {Path.GetFullPath(toolPath)}.");
+
+        //crunch.exe path
+        crunchToolPath = Path.Combine(appData, "crunch.exe");
+        if (!File.Exists(crunchToolPath))
+            throw new FileNotFoundException($"Cannot find dxt encoder at {Path.GetFullPath(crunchToolPath)}.");
+
+        //图片
+        pngPath = Path.Combine(Application.dataPath, "Images", "RGBA32.png");
+        if (!File.Exists(pngPath))
+        {
+            throw new FileNotFoundException($"Cannot find image at {Path.GetFullPath(pngPath)}.");
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            AstcCompressAsync(pngPath).Forget();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            DXTCompressAsync(pngPath).Forget();
+        }
+    }
+
+    async UniTaskVoid AstcCompressAsync(string pngPath)
+    {
+        await AstcCompress(pngPath);
+    }
+
+    async UniTask AstcCompress(string path)
+    {
+        if (!File.Exists(path))
+        {
+            Debug.LogError("File is not exists: " + path);
+            return;
+        }
+
+        int extIndex = path.LastIndexOf('.');
+        string outputPath = (extIndex != -1) ? path.Substring(0, extIndex) + ".astc" : path + ".astc";
+
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = toolPath,
+            Arguments = $" -cl \"{path}\" \"{outputPath}\" 6x6 -medium -yflip",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+        };
+
+        using (Process process = new Process { StartInfo = startInfo })
+        {
+            process.Start();
+            await UniTask.WaitUntil(() => process.HasExited); 
+
+            if (process.ExitCode == 0)
+            {
+                Debug.Log("Texture compressed successfully.");
+            }
+            else
+            {
+                Debug.LogError("Texture compression failed.");
+            }
+        }
+    }
+
+    async UniTaskVoid DXTCompressAsync(string pngPath)
+    {
+        await CrunchCompress(pngPath);
+    }
+
+    async UniTask CrunchCompress(string path)
+    {
+        if (!File.Exists(path))
+        {
+            Debug.LogError("File is not exists: " + path);
+            return;
+        }
+
+        string outputDirectory = Path.Combine(Application.dataPath, "Images");
+        string outputPath = Path.Combine(outputDirectory, Path.GetFileNameWithoutExtension(path) + ".dds");
+
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = crunchToolPath,
+            Arguments = $"-file \"{path}\" -fileformat dds -dxt1",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+            WorkingDirectory = outputDirectory
+        };
+
+        using (Process process = new Process { StartInfo = startInfo })
+        {
+            process.Start();
+
+            string output = await process.StandardOutput.ReadToEndAsync();
+            string error = await process.StandardError.ReadToEndAsync();
+
+            await UniTask.WaitUntil(() => process.HasExited);
+
+            if (process.ExitCode == 0)
+            {
+                Debug.Log("Texture compressed successfully.");
+                Debug.Log("Output File: " + outputPath);
+            }
+            else
+            {
+                Debug.LogError("Texture compression failed.");
+                Debug.LogError("Error: " + error);
+            }
+        }
+    }
+}
